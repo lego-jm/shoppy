4af14979f2134b08fb0d9b9e8ed580090ac069d3
lego_jm <wjddb10@gmail.com>
2023-11-14 22:53:15 +0900

    navbar_publising

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div>
+      <Navigation />
       <Outlet />
     </div>
   );
